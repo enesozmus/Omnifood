13ec9c64820b42dae466c94745669697f1e55f4e
enesozmus <ensozmus@gmail.com>
2022-12-18 14:12:50 +0300

    implementing smooth scrolling using JavaScript

---
+++ js/script.js
@@ -0,0 +1,55 @@
+const myName = "Enes Ozmus";
+/* in case we only have one element */
+const h1 = document.querySelector(".heading-primary");
+
+h1.addEventListener("click", function () {
+  h1.textContent = myName;
+  h1.style.backgroundColor = "white";
+  h1.style.padding = "5rem";
+});
+
+console.log(myName);
+
+// set current year
+const footerYear = document.querySelector(".footer-year");
+const currentYear = new Date().getFullYear();
+footerYear.textContent = currentYear;
+
+// make mobile navigation work
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+// Smooth scrolling animation
+const allLinks = document.querySelectorAll("a:link");
+// console.log(allLinks);
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    // console.log(href);
+
+    // Scroll back to top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
